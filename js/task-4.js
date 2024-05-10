dce85d9691c129bbf21d2940df15663d111ad32d
Paweł <102540215+przytop@users.noreply.github.com>
2024-05-10 19:15:59 +0200

    Initial commit

---
+++ js/task-4.js
@@ -0,0 +1,47 @@
+"use strict";
+
+const allUsersAccounts = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .reduce((totalBalance, balance) => totalBalance + balance.balance, 0);
+
+console.log(getTotalBalanceByGender(allUsersAccounts, "male")); // 12053
+console.log(getTotalBalanceByGender(allUsersAccounts, "female")); // 8863
