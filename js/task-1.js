df3917d6d6164cc224ce9f8a4c4499b4a6c9b8af
Paweł <102540215+przytop@users.noreply.github.com>
2024-05-14 08:43:51 +0200

    Update task-1.js
    fixes

--- js/task-1.js
+++ js/task-1.js
@@ -5,23 +5,28 @@ const customer = {
   balance: 24000,
   discount: 0.1,
   orders: ["Burger", "Pizza", "Salad"],
+
   getBalance() {
     return this.balance;
   },
+
   getDiscount() {
     return this.discount;
   },
+
   setDiscount(value) {
     this.discount = value;
   },
+
   getOrders() {
     return this.orders;
   },
+  
   addOrder(cost, order) {
     this.balance -= cost - cost * this.discount;
     this.orders.push(order);
   },
-};
+}
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); // 0.15
